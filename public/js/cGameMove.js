50eb7861bbf63ae9c1e17b5899736a6e58ad06e4
alyssasimbs <alyssasimbulan1@gmail.com>
2023-03-21 18:36:42 +0000

    21/03/2023 18:36

---
+++ public/js/cGameMove.js
@@ -0,0 +1,64 @@
+const cards = document.querySelectorAll(".card");
+
+let matched = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+
+function flipCard({target: clickedCard}) {
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back img").src,
+        cardTwoImg = cardTwo.querySelector(".back img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matched++;
+        if(matched == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+
+function shuffleCards() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+      let randomPos = Math.floor(Math.random() * cards.length);
+      card.style.order = randomPos;
+    });
+  }
+  
+  window.addEventListener('load', () => {
+    shuffleCards();
+    cards.forEach(card => {
+      card.addEventListener("click", flipCard);
+    });
+  });
+    
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
